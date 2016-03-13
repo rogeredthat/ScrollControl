1fc180ebde1759c9c7cf141d36e05ad3922357ba
Abhilash Verma <Abhilash Verma>
2016-03-14 03:38:19 +0530

    Bounds Created

--- ScrollControl.js
+++ ScrollControl.js
@@ -4,8 +4,17 @@ var step=2;
 var friction=0.5;
 var speed=10;
 document.addEventListener("mousewheel",function(e){
-    if(doc_ScrollTarget)
     doc_ScrollTarget+=(e.wheelDelta*step);
+    if(doc_ScrollTarget<(window.innerHeight-document.body.offsetHeight))
+    {
+        doc_ScrollTarget=(window.innerHeight-document.body.offsetHeight-100);
+        window.setTimeout(function(){doc_ScrollTarget=(window.innerHeight-document.body.offsetHeight);},500);
+    }
+    if(doc_ScrollTarget>0)
+    {
+        doc_ScrollTarget=100;
+        window.setTimeout(function(){doc_ScrollTarget=0;},500);
+    }
 },false);
 
 window.setInterval(function(){
