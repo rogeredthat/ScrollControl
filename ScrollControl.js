59fdfa7c39520adb9dc2aedc961ed933ca84c76b
Abhilash Verma <Abhilash Verma>
2016-03-14 03:46:52 +0530

    Bounds Updated

--- ScrollControl.js
+++ ScrollControl.js
@@ -3,17 +3,18 @@ var FPS=1000;
 var step=2;
 var friction=0.5;
 var speed=10;
+var bounceOff=150;
 document.addEventListener("mousewheel",function(e){
     doc_ScrollTarget+=(e.wheelDelta*step);
     if(doc_ScrollTarget<(window.innerHeight-document.body.offsetHeight))
     {
-        doc_ScrollTarget=(window.innerHeight-document.body.offsetHeight-100);
-        window.setTimeout(function(){doc_ScrollTarget=(window.innerHeight-document.body.offsetHeight);},500);
+        doc_ScrollTarget=(window.innerHeight-document.body.offsetHeight-bounceOff);
+        window.setTimeout(function(){doc_ScrollTarget=(window.innerHeight-document.body.offsetHeight);},200);
     }
     if(doc_ScrollTarget>0)
     {
-        doc_ScrollTarget=100;
-        window.setTimeout(function(){doc_ScrollTarget=0;},500);
+        doc_ScrollTarget=bounceOff;
+        window.setTimeout(function(){doc_ScrollTarget=0;},200);
     }
 },false);
 
